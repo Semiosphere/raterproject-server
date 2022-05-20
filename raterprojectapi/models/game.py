63433a49c098d76081378de97bfe39347f0d690a
Zach Dugger <ztd522@gmail.com>
2022-05-20 12:23:31 -0400

    switching to personal project to practice React

--- raterprojectapi/models/game.py
+++ raterprojectapi/models/game.py
@@ -8,3 +8,8 @@ class Game(models.Model):
     number_of_players = models.CharField(max_length=50)
     est_playtime = models.CharField(max_length=50)
     rec_age = models.CharField(max_length=50)
+    category = models.ManyToManyField("Category", through="Game_Category")
+    
+    #through keyword tells Django to use the table I already created instead of creating a new one when making migrations
+    
+    #on ManyToManyFields, the related table is the table that is being bridged by the key
--- raterprojectapi/views/__init__.py
+++ raterprojectapi/views/__init__.py
@@ -1,1 +1,5 @@
 from .auth import login_user, register_user
+from .games import GameView
+from .ratings import RatingView
+from .reviews import ReviewView
+from .categories import CategoryView
